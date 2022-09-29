1bb3cfa913fc2f5d5aadec90d43ed950dd6f0a62
Ethan Cloin <psnethan@gmail.com>
2022-09-28 21:49:48 -0400

    ad designer partially styled

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -1,7 +1,42 @@
 import "./AdDesigner.css";
 
 const AdDesigner = () => {
-  return <div className="AdDesigner">AdDesigner works</div>;
+  return (
+    <div className="AdDesigner">
+      <h1>Ad Designer</h1>
+      <div className="ad-display-wrapper">
+        <div className="ad-display">
+          <h3>Vote For</h3>
+          <h1>Chocolate</h1>
+        </div>
+      </div>
+      <div className="ad-builder-wrapper">
+        <div className="flavors">
+          <h2>What to Support</h2>
+          <div className="h-list">
+            <button>Chocolate</button>
+            <button>Strawberry</button>
+            <button>Vanilla</button>
+          </div>
+        </div>
+        <div className="themes">
+          <h2>Color Theme</h2>
+          <div className="h-list">
+            <button>Light</button>
+            <button>Dark</button>
+          </div>
+        </div>
+        <div className="font-sizer">
+          <h2>Font Size</h2>
+          <div className="h-list">
+            <button>-</button>
+            <p>42</p>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AdDesigner;
