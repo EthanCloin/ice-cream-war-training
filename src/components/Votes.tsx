160f6b683870b2cf0466d26768af425d35b6fa0e
Ethan Cloin <psnethan@gmail.com>
2022-09-28 22:01:47 -0400

    votes partially styled

--- src/components/Votes.tsx
+++ src/components/Votes.tsx
@@ -1,7 +1,26 @@
 import "./Votes.css";
 
 const Votes = () => {
-  return <div className="Votes">Votes works</div>;
+  return (
+    <div className="Votes">
+      <h1>Vote Here</h1>
+      <div className="h-list">
+        <button>Chocolate</button>
+        <button>Strawberry</button>
+        <button>Vanilla</button>
+      </div>
+      <div className="graph">
+        <p>Chocolate:</p>
+        <div className="graph-row">_</div>
+        <p>Strawberry:</p>
+        <div className="graph-row">_</div>
+        <p>Vanilla:</p>
+        <div className="graph-row">_</div>
+        {/* <div></div> 
+        div with a background color and dependent width*/}
+      </div>
+    </div>
+  );
 };
 
 export default Votes;
