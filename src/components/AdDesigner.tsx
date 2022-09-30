fdb83af2e33a5dd802ee5511015a2684b5e9c404
Ethan Cloin <psnethan@gmail.com>
2022-09-29 21:56:01 -0400

    added some statefulness to designer

--- src/components/AdDesigner.tsx
+++ src/components/AdDesigner.tsx
@@ -1,22 +1,64 @@
+import React, { useState } from "react";
 import "./AdDesigner.css";
 
 const AdDesigner = () => {
+  const [fontSize, setFontSize] = useState(32);
+  const [darkMode, setDarkMode] = useState(false);
+  const [flavorName, setFlavorName] = useState("Pick One");
+
+  const fontIncrementSize = 8;
+  const handleFontUp = () => {
+    setFontSize(fontSize + fontIncrementSize);
+  };
+  const handleFontDown = () => {
+    setFontSize((fontSize) => fontSize - fontIncrementSize);
+  };
+
+  const handleFlavorSelected = (flavor: string) => {
+    // how to grab button text?
+    // take arg of 'flavor: str'
+    setFlavorName(flavor);
+  };
+
+  const adFlavorStyle = {
+    fontSize: fontSize,
+  };
+
+  // const adThemeStyle = {
+  //   backgroundColor: darkMode ? "black" : "white",
+  //   color: darkMode ? "white" : "black",
+  // };
+  // const adThemeClass = darkMode ?
+
   return (
     <div className="AdDesigner">
       <h1>Ad Designer</h1>
-      <div className="ad-display-wrapper">
-        <div className="ad-display">
-          <h3>Vote For</h3>
-          <h1>Chocolate</h1>
-        </div>
+      <div className="ad-display">
+        <h3 className="ad-header">Vote For</h3>
+        <h1 style={adFlavorStyle}>{flavorName}</h1>
       </div>
       <div className="ad-builder-wrapper">
         <div className="flavors">
           <h2>What to Support</h2>
           <div className="h-list">
-            <button>Chocolate</button>
-            <button>Strawberry</button>
-            <button>Vanilla</button>
+            <button
+              onClick={() => handleFlavorSelected("chocolate")}
+              disabled={flavorName === "chocolate"}
+            >
+              Chocolate
+            </button>
+            <button
+              onClick={() => handleFlavorSelected("strawberry")}
+              disabled={flavorName === "strawberry"}
+            >
+              Strawberry
+            </button>
+            <button
+              onClick={() => handleFlavorSelected("vanilla")}
+              disabled={flavorName === "vanilla"}
+            >
+              Vanilla
+            </button>
           </div>
         </div>
         <div className="themes">
@@ -29,9 +71,19 @@ const AdDesigner = () => {
         <div className="font-sizer">
           <h2>Font Size</h2>
           <div className="h-list">
-            <button>-</button>
-            <p>42</p>
-            <button>+</button>
+            <button
+              onClick={handleFontDown}
+              disabled={fontSize === fontIncrementSize}
+            >
+              -
+            </button>
+            <p>{fontSize}</p>
+            <button
+              onClick={handleFontUp}
+              disabled={fontSize === fontIncrementSize * 10}
+            >
+              +
+            </button>
           </div>
         </div>
       </div>
